5187571bb790f16956870cf5de3f4b0f553cd242
Min Joong Kim <minjoong38@gmail.com>
2024-01-16 11:05:24 +0800

    Add init

--- main/main.c
+++ main/main.c
@@ -5,15 +5,23 @@
 #define I2C_M_SCL GPIO_NUM_9
 #define I2C_clk_speed 100000
 
-i2c_config_t config = {
-    .mode = I2C_MODE_MASTER,
-    .sda_io_num = I2C_M_SDA,
-    .sda_pullup_en = GPIO_PULLUP_ENABLE,
-    .scl_io_num = I2C_M_SCL,
-    .scl_pullup_en = GPIO_PULLUP_ENABLE,
-    .master.clk_speed = I2C_clk_speed
-};
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
 
+    i2c_config_t config = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_M_SDA,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_M_SCL,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_clk_speed
+    };
+
+    i2c_param_config(i2c_master_port, &config);
+
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
 
 
 
