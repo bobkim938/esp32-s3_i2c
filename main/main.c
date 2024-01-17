933aea7621755acba63e95c2f31c7ebd247e65bb
Min Joong Kim <minjoong38@gmail.com>
2024-01-17 15:38:24 +0800

    ping-pong

--- main/main.c
+++ main/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "esp_log.h"
-#include "driver/i2c.h"
+#include "driver/i2c.h"\
 
 #define I2C_MASTER_SDA_IO 18 // SDA GPIO_18
 #define I2C_MASTER_SCL_IO 19 // SCL GPIO_19
@@ -22,7 +22,7 @@ static const char *TAG = "i2c-master";
 
 static esp_err_t i2c_master_init(void)
 {
-  
+
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
         .sda_io_num = I2C_MASTER_SDA_IO,
@@ -35,6 +35,7 @@ static esp_err_t i2c_master_init(void)
     if (err != ESP_OK) {
         return err;
     }
+    ESP_LOGI(TAG, "I2C initialized");
     return i2c_driver_install(0, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0); // i2c driver installation (last parameter: interrupt)
 }
 
@@ -58,14 +59,14 @@ static esp_err_t i2c_master_read_slave(uint8_t *data_rd, size_t size, i2c_port_t
 }
 
 
-static esp_err_t i2c_master_send(uint8_t* message, size_t size, i2c_port_t i2c_num)
+static esp_err_t i2c_master_send(uint8_t* msg, size_t size, i2c_port_t i2c_num)
 {
-    ESP_LOGI(TAG, "Sending Message = %d", *message);;   
+    ESP_LOGI(TAG, "Sending Data = %d", *msg);;   
     esp_err_t ret; 
     i2c_cmd_handle_t cmd = i2c_cmd_link_create(); // create i2c command handle    
-    i2c_master_start(cmd); // Add start and stop conditions to the I2C command sequence
-    i2c_master_write_byte(cmd, slave_addr_esp << 1 | WRITE_BIT, ACK_CHECK_EN); // Add write commands to the I2C command sequence
-    i2c_master_write(cmd, message, size, ACK_CHECK_EN); // master write buffer to the i2c bus
+    i2c_master_start(cmd); // add start and stop conditions to the I2C command sequence
+    i2c_master_write_byte(cmd, slave_addr_esp << 1 | WRITE_BIT, ACK_CHECK_EN); // add write commands to the I2C command sequence
+    i2c_master_write(cmd, msg, size, ACK_CHECK_EN); // master write buffer to the i2c bus
     i2c_master_stop(cmd);
     
     ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_PERIOD_MS);
@@ -78,19 +79,17 @@ void app_main(void)
     uint8_t ping = 0;
     uint8_t pong[255] = {0};
     ESP_ERROR_CHECK(i2c_master_init());
-    ESP_LOGI(TAG, "I2C initialized");
 
   while(1)
     {
-        // Sending data
-        i2c_master_send(&ping, sizeof(ping), 0);
+        i2c_master_send(&ping, sizeof(ping), 0); // Sending data
         vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-        // Reading data
-        esp_err_t read_result = i2c_master_read_slave(pong, sizeof(pong), 0);
+        esp_err_t read_result = i2c_master_read_slave(pong, sizeof(pong), 0); // Reading data
         if (read_result == ESP_OK)
         {
             ESP_LOGI(TAG, "Received Data: %d", *pong);
+            ping = *pong + 1;
         }
         else
         {
--- main/slave.c
+++ main/slave.c
@@ -6,71 +6,62 @@
 
 static const char *TAG = "i2c-slave";
 
-#define LED_PIN 2
-
-#define I2C_SLAVE_SDA_IO 18               /*!< gpio number for I2C master data  */
-#define I2C_SLAVE_SCL_IO 19               /*!< gpio number for I2C master clock */
-#define I2C_SLAVE_FREQ_HZ 100000        /*!< I2C master clock frequency */
-#define I2C_SLAVE_TX_BUF_LEN 255                        /*!< I2C master doesn't need buffer */
-#define I2C_SLAVE_RX_BUF_LEN 255                           /*!< I2C master doesn't need buffer */
+#define I2C_SLAVE_SDA_IO 18             
+#define I2C_SLAVE_SCL_IO 19            
+#define I2C_SLAVE_FREQ_HZ 100000       
+#define I2C_SLAVE_TX_BUF_LEN 255                     
+#define I2C_SLAVE_RX_BUF_LEN 255                         
 #define ESP_SLAVE_ADDR 0x0A
 
-#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
-#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
-#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
-#define ACK_VAL 0x0                             /*!< I2C ack value */
-#define NACK_VAL 0x1                            /*!< I2C nack value */
+#define WRITE_BIT I2C_MASTER_WRITE          
+#define READ_BIT I2C_MASTER_READ              
+#define ACK_CHECK_EN 0x1                     
+#define ACK_CHECK_DIS 0x0                 
+#define ACK_VAL 0x0                         
+#define NACK_VAL 0x1                     
 
 int i2c_slave_port = 0;
+
 static esp_err_t i2c_slave_init(void)
 {
-  
     i2c_config_t conf_slave = {
-    .sda_io_num = I2C_SLAVE_SDA_IO,          // select GPIO specific to your project
-    .sda_pullup_en = GPIO_PULLUP_ENABLE,
-    .scl_io_num = I2C_SLAVE_SCL_IO,          // select GPIO specific to your project
-    .scl_pullup_en = GPIO_PULLUP_ENABLE,
-    .mode = I2C_MODE_SLAVE,
-    .slave.addr_10bit_en = 0,
-    .slave.slave_addr = ESP_SLAVE_ADDR,      // address of your project
-    .clk_flags = 0,
+        .sda_io_num = I2C_SLAVE_SDA_IO,          // select GPIO specific to your project
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_SLAVE_SCL_IO,          // select GPIO specific to your project
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .mode = I2C_MODE_SLAVE,
+        .slave.addr_10bit_en = 0,
+        .slave.slave_addr = ESP_SLAVE_ADDR,      // address of your project
+        .clk_flags = 0,
     };
+
     esp_err_t err = i2c_param_config(i2c_slave_port, &conf_slave);
     if (err != ESP_OK) {
         return err;
     }
+    ESP_LOGI(TAG, "I2C Slave initialized");
     return i2c_driver_install(i2c_slave_port, conf_slave.mode, I2C_SLAVE_RX_BUF_LEN, I2C_SLAVE_TX_BUF_LEN, 0);
 }
 
 void app_main(void)
 {
-    uint8_t  received_data[I2C_SLAVE_RX_BUF_LEN] = {0};
-    uint8_t  on_command[] = "LED_ON";
-
-    esp_rom_gpio_pad_select_gpio(LED_PIN);
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    uint8_t pong[I2C_SLAVE_RX_BUF_LEN] = {0};
 
     ESP_ERROR_CHECK(i2c_slave_init());
-    ESP_LOGI(TAG, "I2C Slave initialized successfully");
-    
-    while(1)
+
+    while (1)
     {
-    i2c_slave_read_buffer(i2c_slave_port, received_data, I2C_SLAVE_RX_BUF_LEN, 100 / portTICK_PERIOD_MS);
-    i2c_reset_rx_fifo(i2c_slave_port);
-
-    if(strncmp((const char*)received_data, "LED_ON", 6) == 0)
-    {   
-    ESP_LOGI(TAG, "Data Recived = %s", received_data);
-    gpio_set_level(LED_PIN, 1);
+        // Wait for the master to write data
+        int bytes_received = i2c_slave_read_buffer(i2c_slave_port, pong, I2C_SLAVE_RX_BUF_LEN, 100 / portTICK_PERIOD_MS);
+        if (bytes_received > 0)
+        {
+            for (int i = 0; i < bytes_received; i++)
+            {
+                pong[i] += 1; // Process the received data (incrementing each byte by 1 in this case)
+            }
+            i2c_slave_write_buffer(i2c_slave_port, pong, bytes_received, 100 / portTICK_PERIOD_MS); // Send the modified data back to the master
+            ESP_LOGI(TAG, "Received Data: %d", *pong);
+        }
+        memset(pong, 0, I2C_SLAVE_RX_BUF_LEN); // Clear the received data buffer
     }
-    else if(strncmp((const char*)received_data, "LED_OFF", 7) == 0)
-    {
-    ESP_LOGI(TAG, "Data Recived = %s", received_data);   
-    gpio_set_level(LED_PIN, 0);
-    }
-
-    memset(received_data, 0, I2C_SLAVE_RX_BUF_LEN);
-    }
-
 }
